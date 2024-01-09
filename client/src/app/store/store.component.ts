452ccb9f01a214ca119e520eb65ddb44af03ac70
John Cassidy <jpcassidy@outlook.com>
2024-01-09 11:59:36 -0500

    Update store component to display brands and types to filter products

--- client/src/app/store/store.component.ts
+++ client/src/app/store/store.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CoreComponent } from '../core';
+import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/products';
+import { IType } from '../shared/models/type';
 import { ProductItemsComponent } from './product-items/product-items.component';
 import { SharedCompnent } from '../shared';
+import { StoreParams } from '../shared/models/storeParams';
 import { StoreService } from './store.service';
 
 @Component({
@@ -15,21 +18,58 @@ import { StoreService } from './store.service';
 })
 export class StoreComponent {
   products: IProduct[] = [];
+  brands: IBrand[] = [];
+  types: IType[] = [];
+  storeParams = new StoreParams();
+  totalCount = 0;
 
   constructor(private storeService: StoreService) {}
 
   ngOnInit() {
     this.getProducts();
+    this.GetBrands();
+    this.GetTypes();
   }
 
   private getProducts() {
-    this.storeService.getProducts().subscribe({
+    this.storeService.getProducts(this.storeParams).subscribe({
       next: (response) => {
         this.products = response.data;
+        this.storeParams.pageNumber = response.pageIndex;
+        this.storeParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
         console.log(this.products);
       },
       error: (err) => console.log(err),
-      complete: () => console.log('Catalog API call completed'),
+      complete: () => console.log('Catalog API call to GetProducts completed'),
     });
+  }
+  GetBrands() {
+    this.storeService.getBrands().subscribe({
+      next: (response) => {
+        this.brands = [{ id: '', name: 'All' }, ...response];
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('Catalog API call to GetBrands completed'),
+    });
+  }
+  GetTypes() {
+    this.storeService.getTypes().subscribe({
+      next: (response) => {
+        this.types = [{ id: '', name: 'All' }, ...response];
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('Catalog API call to GetTypes completed'),
+    });
+  }
+  onBrandSelected(brandId: string) {
+    this.storeParams.brandId = brandId;
+    // this.storeParams.pageNumber = 1;
+    this.getProducts();
+  }
+  onTypeSelected(typeId: string) {
+    this.storeParams.typeId = typeId;
+    // this.storeParams.pageNumber = 1;
+    this.getProducts();
   }
 }
--- client/src/app/store/store.service.ts
+++ client/src/app/store/store.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IProduct } from '../shared/models/products';
+import { IType } from '../shared/models/type';
 import { Injectable } from '@angular/core';
+import { StoreParams } from '../shared/models/storeParams';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +15,30 @@ export class StoreService {
 
   baseUrl = 'http://localhost:9010/';
 
-  getProducts() {
+  getProducts(storeParams: StoreParams) {
+    let params = new HttpParams();
+    if (storeParams.brandId) {
+      params = params.append('brandId', storeParams.brandId);
+    }
+    if (storeParams.typeId) {
+      params = params.append('typeId', storeParams.typeId);
+    }
+
+    params = params.append('sort', storeParams.sort);
+    params = params.append('pageIndex', storeParams.pageNumber);
+    params = params.append('pageSize', storeParams.pageSize);
+
     return this.httpClient.get<IPagination<IProduct[]>>(
-      this.baseUrl + 'Catalog/GetAllProducts'
+      this.baseUrl + 'Catalog/GetAllProducts',
+      { params }
     );
+  }
+
+  getBrands() {
+    return this.httpClient.get<IBrand[]>(this.baseUrl + 'Catalog/GetAllBrands');
+  }
+
+  getTypes() {
+    return this.httpClient.get<IType[]>(this.baseUrl + 'Catalog/GetAllTypes');
   }
 }
